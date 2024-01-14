c407543157319632d017c725b64329c17f98488c
volkodavocado <olga.braulova@gmail.com>
2024-01-14 18:17:18 +0300

    fix

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ru.kata.spring.boot_security.demo.models.User;
 
 @Controller
 @RequestMapping("/user")
